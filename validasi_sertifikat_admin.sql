cce83384b73f80d8d6d5840108f9fda25e5d588b
Ucuk <138536893+Ucuk@users.noreply.github.com>
2024-11-21 01:44:14 +0800

    Update validasi_sertifikat_admin.sql

--- validasi_sertifikat_admin.sql
+++ validasi_sertifikat_admin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 20 Nov 2024 pada 18.23
+-- Waktu pembuatan: 20 Nov 2024 pada 18.42
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
